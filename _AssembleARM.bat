2bc7fd69e78090bbefc9a53fe63bb12f73551e0e
Stan H <deindall@outlook.com>
2017-06-24 16:29:41 +0200

    Moved stuff around

--- _AssembleARM.bat
+++ _AssembleARM.bat
@@ -10,7 +10,7 @@ REM EDIT THE FOLLOWING LINES IF YOU WISH TO BUILD THIS AT HOME
 
 REM END OF CUSTOM ENV EDITS
 
-set IncludeDir=%~dp0_CommonASM
+set IncludeDir=%~dp0_StanHaxCommon\asm
 
 @rem Assemble into an elf
 %as% -g -mcpu=arm7tdmi -mthumb-interwork -I "%IncludeDir%" %1 -o "%~n1.elf"
